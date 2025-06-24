48d226882992e9533b3adadd5430d592a84bd7d2
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-06-24 13:54:26 -0300

    atualização

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine, Column, String, Integer, Boolean, Float, ForeignKey
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy_utils.types import ChoiceType
 
 # Cria a conexão
@@ -42,12 +42,15 @@ class Pedido(Base):
     status = Column("status", String)
     usuario = Column("usuario", ForeignKey("usuarios.id"))
     preco = Column("preco", Float)
-    # itens =
+    itens = relationship("ItemPedido", cascade="all, delete")
 
     def __init__(self, usuario, status="PENDENTE", preco=0):
         self.usuario = usuario
         self.preco = preco
         self.status = status
+
+    def calcular_preco(self):
+        self.preco = sum(item.preco_unitario * item.quantidade for item in self.itens)
 
 # Itens Pedidos
 class ItemPedido(Base):
@@ -68,3 +71,8 @@ class ItemPedido(Base):
         self.pedido = pedido
 
 # Executa a criação dos metadados do seu banco (cria efetivamente o banco de dados)
+
+# Migrar o banco de dados
+
+# Criar a migração: alembic revision --autogenerate -m "mensagem"
+# Executar a migração: alembic upgrade head
